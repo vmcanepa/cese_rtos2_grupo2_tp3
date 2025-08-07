66e5c618ae7d814838c49cb61ea309df70a141d6
Victor M. Canepa <canepavictor@gmail.com>
2025-08-07 18:58:22 -0300

    primer commit.

---
+++ README.md
@@ -0,0 +1,69 @@
+# Repositorio de TP 3 del grupo 2 - Sistemas Operativos de Tiempo Real (II)
+
+## 1. Objetivo
+Desarrollar un programa en RTOS que resuelva el problema planteado en el [enunciado de trabajo práctico 3](https://docs.google.com/document/d/1K6UFSMOq2QiGcxp6YNr2O3WbQZQIADuyz9awLDieVfk/edit?tab=t.0#heading=h.8jl34vsv9f2z), usando como plantillas los desarrollos de trabajo práctico 1 y 2 (objetos reactivos; memoria dinámica) y aplicando lo aprendido en el curso sobre **controladores de dispositivos**.
+
+## 2. Integrantes de Grupo 2
+
+- `e2405` -- Jezabel Victoria Danon.
+- `e2406` -- Mariano Ariel Deville.
+- `ext161` -- Victor Manuel Canepa.
+
+## 3. Docentes revisores
+
+- Juan Manuel Cruz.
+- Sebastian Bedin.
+
+ ## 4. Archivos soporte:
+[link al video (INCOMPLETO)](aca-va-el-link)
+
+<!--
+[link al video version segunda entrega](https://drive.google.com/file/d/17ND20xl6OT2Pd_CftyXAzKOlhsYzGrrX/view?usp=sharing)
+-->
+
+---
+
+## 5. Breve guía para trabajo colaborativo (organización del repositorio git)
+
+- **Uso de ramas (`branches`) para evitar pisar cambios entre integrantes:**
+	- **Rama principal [`main`]:** En este _branch_ (rama) solo queda el código **revisado y estable**.
+	- **Ramas de desarrollo [`dev_nombre`]:** Cada integrante puede trabajar libremente en su propia rama. Luego, cuando decide hacer pull a la rama `main` lo hace con _pull request_: es un pull que inicia el usuario pero no se completa hasta que el resto del equipo no se sincroniza).
+
+### 5.1 Pasos a seguir:
+
+- **CREA TU RAMA DE TRABAJO (solo se hace una vez)**:
+
+  - Ejecutá los siguientes comandos `git` en el repositorio:
+    ```bash
+      git checkout main	            # Te aseguras que estás parado en el branch principal (main).
+      git checkout -b dev_NOMBRE    # Desde main, el usuario "NOMBRE" crea una rama para desarrollo (dev).
+      git push origin dev_NOMBRE    # La nueva rama se sube a github.
+    ```
+
+- **SUBIR CAMBIOS DESDE TU RAMA DE TRABAJO**
+
+  Cada uno puede trabajar desde su rama (sin preocuparse de hacer subidas con riesgo de romper la versión revisada y estable):
+
+    - Cada día, al comenzar, haces lo siguiente:
+      ```bash
+      git branch	 # Te dice en que rama estás trabajando actualmente (siempre debería ser la tuya evitando cambios en main).
+      git checkout dev_NOMBRE	 # Si por accidente estás en otra rama, esto te asegura que ahora estás parado en tu branch de desarrollo (dev_NOMBRE).
+      # trabajar normalmente y luego subir a github...
+      git add .
+      git commit -m "tu mensaje que sube a github."
+      git push       # aca subis tus cambios y commit a github, pero con el alivio de que no estas modificando la rama main.
+      ```
+    - Si el grupo actualiza la rama `main` tu rama no sufre cambios. Vos te encargas de traer lo nuevo de `main` a tu rama:
+      ```bash
+      git checkout dev_NOMBRE
+      git merge main  # esto actualiza tu repo local.
+      git push
+      ```
+            
+- **PASAR TUS CAMBIOS A LA RAMA COMÚN (`main`)**
+
+  Cuando tengas mejoras significativas que ya se pueden agregar a la rama `main`:
+  
+  - en github seleccionas la vista de tu rama (`dev_NOMBRE`) y apretás en el botón **Open pull request** Asegurate que el sentido del _pull request_ sea de tu rama a la rama `main`. Listo eso es todo..
+    
+  - ..luego otro integrante recibe el aviso de que se ha iniciado un _pull request_ y solo debe acceder desde github y apretar en **Merge pull request**. Si durante la revisión del _pull request_ se detecta un conflicto de código, arreglarlo antes de hacer **Merge pull request** (o apretar **Close pull request**).
---
+++ f446_grupo_2_tp_3/app/inc/board.h
@@ -0,0 +1,236 @@
+/*
+ * Copyright (c) 2023 Juan Manuel Cruz <jcruz@fi.uba.ar>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file   : board.h
+ * @date   : Set 26, 2023
+ * @author : Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ * @version	v1.0.0
+ */
+
+#ifndef BOARD_INC_BOARD_H_
+#define BOARD_INC_BOARD_H_
+
+/********************** CPP guard ********************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************** inclusions *******************************************/
+#include "stm32f4xx_hal_gpio.h"
+/********************** macros ***********************************************/
+#define NUCLEO_F103RC		(0)
+#define NUCLEO_F401RE		(1)
+#define NUCLEO_F446RE		(2)
+#define NUCLEO_F429ZI		(3)
+#define NUCLEO_F439ZI		(4)
+#define NUCLEO_F413ZH		(5)
+#define STM32F429I_DISC1	(6)
+#define STM32F407G_DISC1	(7)
+
+/* GRUPO2_VIC: Debe definirse desde argumento en compilador. */
+#ifndef GRUPO2_VIC
+	#define GRUPO2_446
+#endif
+
+
+#ifdef GRUPO2_446
+	/* Aca van los defines que activan Jezabel y Mariano para su configuracion: */
+	#define BOARD (NUCLEO_F446RE)
+	#define BOTONERA	/* tomar los pines de la botonera */
+
+#elif defined(GRUPO2_VIC)
+	/* Aca van los defines que activa Victor para su configuracion: */
+	#define BOARD (STM32F407G_DISC1)
+
+#endif
+
+
+/* STM32 Nucleo Boards - 64 Pins */
+#if ((BOARD == NUCLEO_F103RC) || (BOARD == NUCLEO_F401RE) || (BOARD == NUCLEO_F446RE))
+
+# ifndef BOTONERA
+	#define BTN_A_PIN	B1_Pin
+	#define BTN_A_PORT	B1_GPIO_Port
+	#define BTN_B_PIN	B1_Pin
+	#define BTN_B_PORT	B1_GPIO_Port
+	#define BTN_C_PIN	B1_Pin
+	#define BTN_C_PORT	B1_GPIO_Port
+
+	#define LED_A_PIN	LD2_Pin
+	#define LED_A_PORT	LD2_GPIO_Port
+	#define LED_B_PIN	LD2_Pin
+	#define LED_B_PORT	LD2_GPIO_Port
+	#define LED_C_PIN	LD2_Pin
+	#define LED_C_PORT	LD2_GPIO_Port
+#endif /* not BOTONERA */
+
+#define BTN_PRESSED	GPIO_PIN_RESET
+#define BTN_HOVER	GPIO_PIN_SET
+
+#ifdef GRUPO2_446
+	// Leds pullup
+	#define LED_ON		GPIO_PIN_RESET
+	#define LED_OFF		GPIO_PIN_SET
+#else
+	#define LED_ON		GPIO_PIN_SET
+	#define LED_OFF		GPIO_PIN_RESET
+#endif
+
+# ifdef BOTONERA
+	// pines y leds de la botonera
+	#define LD1_Pin GPIO_PIN_6
+	#define LD1_GPIO_Port GPIOA
+	#define LD2_Pin GPIO_PIN_5
+	#define LD2_GPIO_Port GPIOA
+	#define LD3_Pin GPIO_PIN_7
+	#define LD3_GPIO_Port GPIOA
+	#define BT1_Pin GPIO_PIN_9
+	#define BT1_GPIO_Port GPIOA
+	#define BT2_Pin GPIO_PIN_6
+	#define BT2_GPIO_Port GPIOB
+	#define BT3_Pin GPIO_PIN_7
+	#define BT3_GPIO_Port GPIOC
+
+	#define LED_RED_PIN		LD1_Pin
+	#define LED_BLUE_PIN	LD2_Pin
+	#define LED_GREEN_PIN	LD3_Pin
+	#define LED_RED_PORT	LD1_GPIO_Port
+	#define LED_BLUE_PORT	LD2_GPIO_Port
+	#define LED_GREEN_PORT	LD3_GPIO_Port
+
+	#define BTN_PIN		B1_Pin
+	#define BTN_PORT	B1_GPIO_Port
+
+#endif /* BOTONERA */
+
+#endif /* STM32 Nucleo Boards - 64 Pins */
+
+/* STM32 Discovery Kits */
+#if (BOARD == STM32F407G_DISC1)
+
+#define BTN_A_PIN	B1_Pin
+#define BTN_A_PORT	B1_GPIO_Port
+#define BTN_B_PIN	B1_Pin
+#define BTN_B_PORT	B1_GPIO_Port
+#define BTN_C_PIN	B1_Pin
+#define BTN_C_PORT	B1_GPIO_Port
+
+#define BTN_PRESSED	GPIO_PIN_SET
+#define BTN_HOVER	GPIO_PIN_RESET
+
+#define LED_A_PIN	LD3_Pin
+#define LED_A_PORT	LD3_GPIO_Port
+#define LED_B_PIN	LD4_Pin
+#define LED_B_PORT	LD4_GPIO_Port
+#define LED_C_PIN	LD5_Pin
+#define LED_C_PORT	LD5_GPIO_Port
+
+#define LED_ON		GPIO_PIN_SET
+#define LED_OFF		GPIO_PIN_RESET
+
+#ifndef BOTONERA
+	#define BTN_PIN         BTN_A_PIN
+	#define BTN_PORT        BTN_A_PORT
+
+	#define LED_RED_PIN     LED_C_PIN
+	#define LED_RED_PORT    LED_C_PORT
+	#define LED_GREEN_PIN   LED_A_PIN
+	#define LED_GREEN_PORT  LED_A_PORT
+	#define LED_BLUE_PIN    LED_B_PIN
+	#define LED_BLUE_PORT   LED_B_PORT
+#endif /* not BOTONERA */
+
+#endif /* STM32 Discovery Kits */
+
+/* STM32 Nucleo Boards - 144 Pins */
+#if ((BOARD == NUCLEO_F429ZI) || (BOARD == NUCLEO_F439ZI) || (BOARD == NUCLEO_F413ZH))
+
+#define BTN_A_PIN	USER_Btn_Pin
+#define BTN_A_PORT	USER_Btn_GPIO_Port
+#define BTN_B_PIN	USER_Btn_Pin
+#define BTN_B_PORT	USER_Btn_GPIO_Port
+#define BTN_C_PIN	USER_Btn_Pin
+#define BTN_C_PORT	USER_Btn_GPIO_Port
+
+#define BTN_PRESSED	GPIO_PIN_SET
+#define BTN_HOVER	GPIO_PIN_RESET
+
+#define LED_A_PIN	LD1_Pin
+#define LED_A_PORT	LD1_GPIO_Port
+#define LED_B_PIN	LD2_Pin
+#define LED_B_PORT	LD2_GPIO_Port
+#define LED_C_PIN	LD3_Pin
+#define LED_C_PORT	LD3_GPIO_Port
+
+#define LED_ON		GPIO_PIN_SET
+#define LED_OFF		GPIO_PIN_RESET
+
+#endif /* STM32 Nucleo Boards - 144 Pins */
+
+/* STM32 Discovery Kits */
+#if (BOARD == STM32F429I_DISC1)
+
+#define BTN_A_PIN	B1_Pin
+#define BTN_A_PORT	B1_GPIO_Port
+#define BTN_B_PIN	B1_Pin
+#define BTN_B_PORT	B1_GPIO_Port
+#define BTN_C_PIN	B1_Pin
+#define BTN_C_PORT	B1_GPIO_Port
+
+#define BTN_PRESSED	GPIO_PIN_SET
+#define BTN_HOVER	GPIO_PIN_RESET
+
+#define LED_A_PIN	LD3_Pin
+#define LED_A_PORT	LD3_GPIO_Port
+#define LED_B_PIN	LD4_Pin
+#define LED_B_PORT	LD4_GPIO_Port
+#define LED_C_PIN	LD4_Pin
+#define LED_C_PORT	LD4_GPIO_Port
+
+#define LED_ON		GPIO_PIN_SET
+#define LED_OFF		GPIO_PIN_RESET
+
+#endif
+
+/********************** typedef **********************************************/
+
+/********************** external data declaration ****************************/
+
+/********************** external functions declaration ***********************/
+
+/********************** End of CPP guard *************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_INC_BOARD_H_ */
+
+/********************** end of file ******************************************/
